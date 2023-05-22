b38b9dca5014f00dd56daef1905f57c21d510741
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-05-22 16:12:20 +0200

    Layout

--- Controls/MainWindow.xaml.cs
+++ Controls/MainWindow.xaml.cs
@@ -25,49 +25,66 @@ namespace Controls
             InitializeComponent();
         }
 
+        //Event-Handler für das Click-Event des Buttons
         private void Btn_KlickMich_Click(object sender, RoutedEventArgs e)
         {
-            if (Cbx_Haken.IsChecked == true)
-                Btn_KlickMich.Content = "Wurde geklickt";
-
+            //Übertrag des TextBox-Texts in den Label-Content
             Lbl_Output.Content = Tbx_Input.Text;
 
-            if (Cbb_Auswahl.SelectedItem != null)
-                Tbl_Output.Text = (Cbb_Auswahl.SelectedItem as ComboBoxItem).Content.ToString();
-        }
+            //Änderung der Hintergrundfarbe des Buttons
+            Btn_KlickMich.Background = new SolidColorBrush(Colors.Red);
 
-        private void Sdr_Wert_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            Lbl_Output.Content = Sdr_Wert.Value.ToString();
+            //Neuzuweisung der Text-Eigenschaft des TextBlocks mit dem ausgewählten Inhalt der ComboBox
+            Tbl_Output.Text = Cbb_Auswahl.SelectedItem.ToString();
+
+            //Prüfung, ob die Checkbox abgehakt ist
+            if (Cbx_Haken.IsChecked == true)
+                //Übertrag des Slider-Values in den Button-Content
+                Btn_KlickMich.Content = Sdr_Wert.Value;
         }
 
         private void Schließen_Click(object sender, RoutedEventArgs e)
         {
-            if(MessageBox.Show("Möchtest du das Fenster wirklich schließen?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            //Anzeigen einer MessageBox und Abfrage des geklickten Buttons
+            if (MessageBox.Show
+                 (
+                     "Soll das Fenster wirklich geschlossen werden?",
+                     "Programm beenden",
+                     MessageBoxButton.YesNo,
+                     MessageBoxImage.Question
+                 ) == MessageBoxResult.Yes)
+                //Schließen des Fensters
                 this.Close();
+
+            //Beenden der Applikation
+            //Application.Current.Shutdown();
         }
 
         private void Neu_Click(object sender, RoutedEventArgs e)
         {
             Window wnd = new MainWindow();
+
             wnd.Title = "Neues Fenster";
 
+            //Öffen eines neuen Fensters als gleichberechtigtes Fenster
             wnd.Show();
         }
 
         private void Dialog_Click(object sender, RoutedEventArgs e)
         {
             Window wnd = new MainWindow();
+
             wnd.Title = "Neues Dialog-Fenster";
 
+            //Öffnen eines neuen Fensters als Dialogfenster mit Rückgabe des DialogResults
             if (wnd.ShowDialog() == true)
-                Lbl_Output.Content = "OKAY!!!!!";
+                Lbl_Output.Content = "OKAY!!!";
         }
 
-        private void Btn_Ok_Click(object sender, RoutedEventArgs e)
+        private void Btn_ok_Click(object sender, RoutedEventArgs e)
         {
+            //Setzen des DialogResults (NUR IN DIALOGFENSTERN) und automatisches Schließen des Fensters
             this.DialogResult = true;
-            this.Close();
         }
     }
 }
--- Grundlagen/MainWindow.xaml.cs
+++ Grundlagen/MainWindow.xaml.cs
@@ -18,25 +18,22 @@ namespace Grundlagen
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainFenster : Window
+    public partial class MainWindow : Window
     {
-        public MainFenster()
+        public MainWindow()
         {
+            //Dies verweist auf eine Methode in der (versteckten) automatisch generierten zweiten Klassen-Datei (*.g.i.cs),
+            //welche für das Rendering des XAML-Codes verantwortlich ist. InitializeComponent() erstellt die
+            //Steuerelement-Objekte und muss daher als erste Methode des Konstruktors bestehen bleiben
             InitializeComponent();
-
-            Btn_KlickMich.Background = new SolidColorBrush(Colors.Blue);
         }
 
-        private void Btn_KlickMich_Click(object sender, RoutedEventArgs e)
+        private void Btn_BeispielButton_Click(object sender, RoutedEventArgs e)
         {
-            (sender as Button).Content = "Wurde geklickt";
-
-            Btn_KlickMich.Background = new SolidColorBrush(Colors.Green);
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            this.Title = (sender as TextBox).Text;
+            //Verändern des Inhalts des Buttons
+            Btn_BeispielButton.Content = "Ich wurde angeklickt";
+            //Ändern einer UI-Property über den Sender-Parameter (beinhaltet den Verursacher des Events)
+            (sender as Button).Background = new SolidColorBrush(Colors.Orange);
         }
     }
 }

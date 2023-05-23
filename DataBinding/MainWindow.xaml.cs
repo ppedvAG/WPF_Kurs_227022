84dd5ea607d10ab1f62f6d5d2d93702fd93ac259
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-05-23 13:51:32 +0200

    DataBinding

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void Btn_Show_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = Spl_DataContextBsp.DataContext as Person;
+            MessageBox.Show($"{person.Vorname} {person.Nachname} ({person.Alter})");
+        }
+
+        private void Btn_Altern_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = Spl_DataContextBsp.DataContext as Person;
+            person.Alter++;
+            person.WichtigeTage.Add(new DateTime(2000, 1, 1));
+
+            person.UpdateGUI();
+
+        }
+
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person(){Vorname="Anna", Nachname="Nass", Alter=45},
+            new Person(){Vorname="Rainer", Nachname="Zufall", Alter=31},
+            new Person(){Vorname="Maria", Nachname="Maier", Alter=12},
+        };
+
+        private void Btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (Lsb_Personen.SelectedItem != null)
+                Personenliste.Remove(Lsb_Personen.SelectedItem as Person);
+        }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            Personenliste.Add(new Person() { Vorname = "Hannes", Nachname = "Schmidt", Alter = 65 });
+        }
+    }
+}
---
+++ DataBinding/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+
+        public List<DateTime> WichtigeTage { get; set; } = new List<DateTime>()
+        {
+            DateTime.Now,
+            new DateTime(2002, 3, 1),
+            new DateTime(2023, 1, 23),
+            new DateTime(1997, 12, 7),
+        };
+
+        public DateTime LetzterTag { get { Thread.Sleep(1000); return WichtigeTage.Last(); } }
+
+        public Person()
+        {
+            Alter = 31;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void UpdateGUI()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LetzterTag)));
+        }
+
+        public override string ToString()
+        {
+            return $"{Vorname} {Nachname} ({Alter})";
+        }
+    }
+}

86e6128cdc48fe606e770caedb6e55ec24c119ea
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-05-24 10:18:50 +0200

    Templates

---
+++ Templates/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button funktioniert");
+
+        }
+
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person() {Vorname="Anna", Nachname="Nass", Alter=34},
+            new Person() {Vorname="Rainer", Nachname="Zufall", Alter=67 },
+            new Chef(){Vorname="Hannes", Nachname="Maier", Alter=87, Gehalt=6000}
+        };
+
+        private void Btn_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Personenliste.Remove((sender as Button).Tag as Person);
+        }
+    }
+}
---
+++ Templates/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Templates
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+
+        public DateTime Geburtsdatum { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person: {Vorname}";
+        }
+    }
+
+    public class Chef : Person
+    {
+        public int Gehalt { get; set; }
+    }
+}

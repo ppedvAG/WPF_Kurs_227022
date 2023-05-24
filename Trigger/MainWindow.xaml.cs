1e5c5a43fd7cabd4808f19211deb92b75f056b0c
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-05-24 16:32:22 +0200

    UserControls

---
+++ Trigger/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trigger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Setzen des DataContext
+            this.DataContext= this;
+        }
+
+        public bool BoolVal { get; set; } = false;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //EventHandler zum Ändern der Property
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BoolVal = !BoolVal;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal)));   
+        }
+
+        private void colorpicker_PickedColorChanged(object sender, RoutedPropertyChangedEventArgs<SolidColorBrush> e)
+        {
+            if ((e.NewValue as SolidColorBrush).Color.R == 255)
+                MessageBox.Show("Test bestanden");
+        }
+    }
+}
